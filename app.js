c6059fb3a7eb48955020b26853a4ebe26c1aebce
LuisMartinez <moises1210@hotmail.com>
2022-11-30 20:49:04 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+//function sum
+
+function sum(num1, num2) {
+
+    let total = 0
+
+    total = num1 + num2
+
+    return total
+}
+const res = sum(11, 22222)
+console.log(res); 
+
+
+//funtion subtract
+function subtract(num1, num2){
+
+    let total
+
+    total = num1 - num2
+
+    return  total
+}
+
+const resta = subtract(222, 22)
+console.log(resta);
+
+//function multiply
+
+function multiply (num1, num2){
+
+    let total
+
+    total = num1 * num2 
+
+    return total
+}
+
+const multi = multiply(12, 3242)
+console.log(multi);
+//function divide
+
+function divide (num1, num2){
+
+    let total
+
+    total = num1 / num2 
+
+    return total
+}
+
+const divi = divide(3422, 42)
+console.log(divi);
+
+
